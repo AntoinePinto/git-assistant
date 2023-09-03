bd639020021d46accb20e37fe6611a9fa8b55852
Antoine Pinto <antoine.pinto1@outlook.fr>
2023-09-03 15:20:04 +0200

    Update requirements version

--- setup.py
+++ setup.py
@@ -4,13 +4,21 @@ from setuptools import setup, find_packages
 
 setup(
     name="git-assistant",
-    version="1.0.1",
+    version="1.0.2",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
             "git-assistant = git_assistant:main"
         ]
     },
-    install_requires=["pwinput", "easyenvi", "nbformat", "nbconvert", "openai", "GitPython"
+    install_requires=[
+        "easyenvi==1.0.4", 
+        "fsspec==2023.6.0",
+        "GitPython==3.1.32",
+        "nbconvert==7.8.0",
+        "nbformat==5.9.2",  
+        "numpy==1.24.4",   
+        "openai==0.27.10",
+        "pwinput==1.0.3"
     ],
 )
